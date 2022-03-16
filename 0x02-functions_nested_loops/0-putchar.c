cd67ea645e8250c20393e48322ed3417b103bfde
abraham01010 <abrahamenat123@gmail.com>
2022-03-16 06:54:34 -0700

    first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,20 @@
-#include 'main.h'
-
+#include "main.h"
 /**
- * main - Entry point
+ * main - Prints _putchar
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
 
-	_putchar(_);
-	_putchar(p);
-	_putchar(u);
-	_putchar(t);
-	_putchar(c);
-	_putchar(h);
-	_putchar(a);
-	_putchar(r);
-	_putchar ('\n');
-
-	return (0);
+  _putchar('\n');
+  return (0);
 }
-
