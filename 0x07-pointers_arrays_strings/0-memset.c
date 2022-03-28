a6fb728b3d9754d62f9cefae1958055b145d0608
abraham01010 <abrahamenat123@gmail.com>
2022-03-28 11:08:44 -0800

    first commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,16 @@
-#include "main"
-
+#include "main.h"
 /**
- * _memset - prints buffer in hexa
- * @s: buffer
- * @b: write
- * @n: size
- * Return: Nothing.
+ * _memset - function that fills memory with a constant byte
+ * @s: is the direction you are going
+ * @b: constant character variable
+ * @n: is the limit to which you will reach
+ * Return: s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
 		*(s + i) = b;
-	}
 	return (s);
 }
-
