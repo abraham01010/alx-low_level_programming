146ccdd9184cad808235c0c7fffd3dfa3f023e37
abraham01010 <abrahamenat123@gmail.com>
2022-03-31 06:18:20 -0800

    first commit

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * main - check the code
- * 
- * Return: Always 0.
+ * factorial - returns the factorial of a given number
+ * @n: input integer
+ * Return: -1 if n lower than 0, else return factorial
  */
 
 int factorial(int n)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
-	if (n == 1)
+	if (n == 0)
 	{
 		return (1);
 	}
-	return (n * factorial(n - 1));
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
 }
